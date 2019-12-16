886891faf1d25ee07fdc1fa0f526e7418fc2ba4e
Ugudei <ugudeit@gmail.com>
2019-12-16 13:39:23 -0700

    Update HoneyMakerBee.js

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee{
+  constructor(age,job){
+
+    super(age,job);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
   // TODO..
+  };
+  makeHoney() {
+    return this.honeyPot++;
+  }
+  giveHoney() {
+    return this.honeyPot--;
+  }
 };
