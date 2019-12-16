5113c2a2f0fdc6a56394626f5b53a8f21d39fe12
Ugudei <ugudeit@gmail.com>
2019-12-16 13:39:36 -0700

    Update RetiredForagerBee.js

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,19 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee {
   // TODO..
+  constructor(age,job, color){
+
+    super(age,job, color);
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+    this.treasureChest = [];
+  // TODO..
+  };
+  forage(){
+    return 'I am too old, let me play cards instead'
+  }
+  gamble(treasure) {
+    return this.treasureChest.push(treasure)
+  }
 };
