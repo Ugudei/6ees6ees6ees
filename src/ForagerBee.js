6d5150c647043ec2319f3f36e9791a1597e92666
Ugudei <ugudeit@gmail.com>
2019-12-16 13:39:09 -0700

    Update ForagerBee.js

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
+  constructor(age,job){
+
+    super(age,job);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  // TODO..
+  };
+  forage(treasure) {
+    return this.treasureChest.push(treasure)
+  }
 };
